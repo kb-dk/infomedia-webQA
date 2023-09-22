aa31a8caec19d08be11d2e24b949d807bb7da594
Patrick Bruun <pabr@kb.dk>
2023-09-22 13:52:49 +0200

    Fixed routing and added env for base_url

--- src/js/vue.config.js
+++ src/js/vue.config.js
@@ -1,18 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: './',
-
-  assetsDir: 'static',
-  indexPath: 'index.html',
-  chainWebpack: config => {
-    config
-        .plugin('html')
-        .tap(args => {
-          args[0].template = './public/index.html'
-          args[0].title = 'infomedia-webqa'
-          return args
-        })
-  }
+    transpileDependencies: true,
+    publicPath: process.env.BASE_URL,
+    // publicPath: '/infomedia-webqa/',
+    // publicPath: './',
+    assetsDir: 'static',
+    indexPath: 'index.html',
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].template = './public/index.html'
+                args[0].title = 'infomedia-webqa'
+                return args
+            })
+    }
 })
 
