f65249a3ac83108d9c8df6f9a92681701c07f0c2
Patrick Bruun <pabr@kb.dk>
2024-03-11 11:12:51 +0100

    Added roundtrip dropdown to switch between roundtrips to display.

--- src/components/BatchMetadata.vue
+++ src/components/BatchMetadata.vue
@@ -2,7 +2,17 @@
   <div v-if="batchMetadata.showBatch.value" class="batchMetadata" :class="{'is-active': batchMetadata.showBatch}" @click.stop>
     <h2 v-text="batchMetadata.currentDay.value" @click.stop></h2>
     <ApproveButton :stateChange="'technicalQAapproved'" :btnText="'Approve Batch'" @click.stop></ApproveButton>
+    <div id="errorListDiv">
     <PdfErrorList :date="batchMetadata.currentDay.value"></PdfErrorList>
+    </div>
+    <div id="roundtripDropdown">
+      <b-dropdown :text="dropdownText" dropleft >
+        <b-dropdown-item  v-for="roundtrip in getRoundtrips()" :key="roundtrip" @click="dropdownText = roundtrip.name" :class=" roundtrip.name === dropdownText ? 'roundtripDropdownActive' : '' ">
+          {{roundtrip.name}}
+        </b-dropdown-item>
+      </b-dropdown>
+    </div>
+
   </div>
   <div @click.stop v-if="batchMetadata.showBatch.value" class="batchMetadata batchMetadataNotes">
     <NotesForm @click.stop></NotesForm>
@@ -22,7 +32,11 @@ export default defineComponent({
     PdfErrorList,
     NotesForm
   },
-
+  data(){
+    return{
+      dropdownText:ref("rt4")
+    }
+  },
   setup(){
     const batchMetadata={
       currentDay: ref("null"),
@@ -46,6 +60,9 @@ export default defineComponent({
       this.batchMetadata.showBatch.value = true
       this.batchMetadata.currentDay.value = event.id
     },
+    getRoundtrips(date =""){
+      return[{name:"rt1"},{name:"rt2"},{name:"rt3"},{name:"rt4"}]
+    }
   }
 
 
