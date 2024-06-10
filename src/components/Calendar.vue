3cd472e36a6f1cb9c746d7202e09c593d10b8bc1
Patrick Bruun <pabr@kb.dk>
2024-06-10 16:32:17 +0200

    Fixed errors. Now reloads data if new newspaper has been selected

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -4,7 +4,8 @@
                 @date-select="yearSelect" id v-model="date" view="year" dateFormat="yy" class="yearPicker">
     </YearPicker>
     <!--  <DatePicker :masks="format"/>-->
-    <Calendar locale="da" :initialPage="{day: isYear ? 1: date.getDay(),month: isYear ? 1 : date.getMonth()+1,year:date.getFullYear()}"
+    <Calendar locale="da"
+              :initialPage="{day: isYear ? 1: date.getDay(),month: isYear ? 1 : date.getMonth()+1,year:date.getFullYear()}"
               :masks="monthMask" :rows="rows" ref="yearCalendar" class="yearCalendar"
               :columns="columns" :attributes="calendarAttr" :first-day-of-week="2" :nav-visibility="monthNav"
               expanded
@@ -12,7 +13,7 @@
               @dayclick="calendarDayClicked"
               @update:pages.once="isYear ? batchesForYear() : batchesForMonth()"
     />
-<!--    {{batchesForMonth()}}-->
+    <!--    {{batchesForMonth()}}-->
     <!--  <Calendar v-model="date"></Calendar>-->
   </div>
 
@@ -29,134 +30,146 @@ import 'v-calendar/style.css'
 // import "src/style/stylesheet.scss";
 
 export default defineComponent({
-  name: "year-calendar",
-  components: {
-    Calendar,
-    YearPicker,
-  },
-  props: {
-    isYear: [Boolean],
-    rows: [Number],
-    columns: [Number],
-    monthNav: [String],
-    monthMask: [Object],
-    batchType:[String],
-    newspaperName:[String]
-  },
-  data() {
-    return {
-      currentYear: 2023,
-      date: ref(new Date()),
-      yearPickerInputStyle: {'text-align': 'center', 'font-size': 'larger', 'font-weight': 'bold'},
-      calendarAttr:ref([{}])
-    }
-  },
-  watch:{
-    batchType(newVal){
-      this.calendarAttr = [{}]
-      this.batchesForMonth();
-    }
-  },
-
-  methods: {
-    yearSelect() {
-      let createdDate = this.date;
-      this.$refs.yearPicker.updateModel(createdDate)
-      this.$refs.yearCalendar.focusDate(createdDate)
-
-    },
-    next(event) {
-      this.loading = true
-
-      // this.date.setMonth(event[0].month - 1);
-      // this.date.setYear(event[0].year)
-      if (this.isYear) {
-        this.date = new Date(event[0].year,0,1)
-        this.$refs.yearPicker.updateModel(this.date)
-        console.log("h")
-      } else {
-        this.date = new Date(event[0].year,event[0].month-1,1)
-        this.$forceUpdate();
-        this.batchesForMonth()
-      }
-
-    },
-    calendarDayClicked(calendarData, event) {
-      if (this.isYear) {
-        this.$router.push({
-          name: "newspaper-view",
-          params: {
-            batchid: "dl_20210101_rt1",
-            newspaperid: "Aarhusstiftidende",
-            year: calendarData.year,
-            month: calendarData.month,
-            day: calendarData.day
-          }
-        })
-      } else {
-        event.stopPropagation()
-        // console.log(calendarData)
-        this.$parent.showBatchInfo(calendarData)
-      }
-
-    },
-    async batchesForMonth() {
-      let res = [];
-      const apiClient = axios.create({
-        baseURL: '/api',
-      })
-      const {data} = await apiClient.get(`/batches?month=${this.date.getMonth()+1}&year=${this.date.getFullYear()}&batch_type=${this.batchType}`)
-      res = data
-
-      if (res.length > 0) {
-        for (let i = 0; i < res.length; i++) {
-          const {data} = await apiClient.get(`/batches/${res[i].id}/has_problems`);
-          res[i] = {
-            highlight: {
-              color: data ? 'red':'teal',
-              fillMode: this.pickFillMode(res[i].state)
-            },
-            dates: new Date(res[i].date),
-            popover: null,
-            datePicker: null,
-            batch:res[i]
-
-          }
+      name: "year-calendar",
+      components: {
+        Calendar,
+        YearPicker,
+      },
+      props: {
+        isYear: [Boolean],
+        rows: [Number],
+        columns: [Number],
+        monthNav: [String],
+        monthMask: [Object],
+        batchType: [String],
+        newspaperName: [String]
+      },
+      data() {
+        return {
+          currentYear: 2023,
+          date: ref(new Date()),
+          yearPickerInputStyle: {'text-align': 'center', 'font-size': 'larger', 'font-weight': 'bold'},
+          calendarAttr: ref([{}])
         }
-        this.calendarAttr = res
-      }
-    },
-    updateBatchState(batchName,newState){
-      for(let i=0; i<this.calendarAttr.length; i++) {
-        if(this.calendarAttr[i].batch.batch_name === batchName){
-          this.calendarAttr[i].batch.state = newState
-          this.calendarAttr[i].highlight.fillMode = this.pickFillMode(newState)
+      },
+      watch: {
+        batchType(newVal) {
+          this.calendarAttr = [{}]
+          this.batchesForMonth();
+        },
+        newspaperName(newVal){
+          this.calendarAttr[{}];
+          this.batchesForYear();
         }
+      },
+
+      methods: {
+        yearSelect() {
+          let createdDate = this.date;
+          this.$refs.yearPicker.updateModel(createdDate)
+          this.$refs.yearCalendar.focusDate(createdDate)
+
+        },
+        next(event) {
+          this.loading = true
+
+          // this.date.setMonth(event[0].month - 1);
+          // this.date.setYear(event[0].year)
+          if (this.isYear) {
+            this.date = new Date(event[0].year, 0, 1)
+            this.$refs.yearPicker.updateModel(this.date)
+            console.log("h")
+          } else {
+            this.date = new Date(event[0].year, event[0].month - 1, 1)
+            this.$forceUpdate();
+            this.batchesForMonth()
+          }
+
+        },
+        calendarDayClicked(calendarData, event) {
+          if (this.isYear) {
+            this.$router.push({
+              name: "newspaper-view",
+              params: {
+                batchid: "dl_20210101_rt1",
+                newspaperid: "Aarhusstiftidende",
+                year: calendarData.year,
+                month: calendarData.month,
+                day: calendarData.day
+              }
+            })
+          } else {
+            event.stopPropagation()
+            // console.log(calendarData)
+            this.$parent.showBatchInfo(calendarData)
+          }
+
+        },
+        async batchesForMonth() {
+          let res = [];
+          const apiClient = axios.create({
+            baseURL: '/api',
+          })
+          const {data} = await apiClient.get(`/batches?month=${this.date.getMonth() + 1}&year=${this.date.getFullYear()}&batch_type=${this.batchType}&get_latest=true`)
+          res = data
+
+          if (res.length > 0) {
+            for (let i = 0; i < res.length; i++) {
+              const {data} = await apiClient.get(`/batches/${res[i].id}/has_problems`);
+              res[i] = {
+                highlight: {
+                  color: data ? 'red' : 'teal',
+                  fillMode: this.pickFillMode(res[i].state)
+                },
+                dates: new Date(res[i].date),
+                popover: null,
+                datePicker: null,
+                batch: res[i]
+
+              }
+            }
+            this.calendarAttr = res
+          }
+        },
+        updateBatchState(batchName, newState) {
+          for (let i = 0; i < this.calendarAttr.length; i++) {
+            if (this.calendarAttr[i].batch.batch_name === batchName) {
+              this.calendarAttr[i].batch.state = newState
+              this.calendarAttr[i].highlight.fillMode = this.pickFillMode(newState)
+            }
+          }
+        },
+        pickFillMode(state) {
+          return state === 'QAChecked' ? 'solid' : state === 'BatchInspected' ? 'light' : 'outline'
+        },
+        async batchesForYear() {
+          let res = [];
+          const {data} = await axios.get(`/api/batches?state=TechnicalInspectionComplete&year=${this.date.getFullYear()}&newspaper_name=${this.newspaperName}&get_latest=true`);
+          res = data;
+          for (let i = 0; i < res.length; i++) {
+            const newspapers = (await axios.get(`/api/batches/${res[i].id}/newspapers?newspaper_name=${this.newspaperName}`)).data
+            for(let j = 0; j < newspapers.length; j++) {
+              res[i] = {
+                highlight: {
+                  color: 'teal',
+                  fillMode: newspapers[j].checked?'light':'solid'
+                },
+                dates: new Date(res[i].date),
+                popover: null,
+                datePicker: null,
+                batch: res[i]
+
+              }
+            }
+            }
+
+          this.calendarAttr = res;
+        }
+
       }
-    },
-    pickFillMode(state){
-      return state === 'QAChecked'? 'solid': state === 'BatchInspected'? 'light':'outline'
-    },
-    async batchesForYear(){
-      let res = [];
-      // const {data} = await axios.get(`/api/batches?state=QAChecked&year=${this.date.getFullYear()}&newspaper_name=${this.newspaperName}&get_latest=true`);
-      // res = data;
-      // for(let i=0; i<res.length; i++) {
-      //   res[i] = {
-      //     highlight: {
-      //       color:'teal',
-      //       fillMode:̈́'solid'
-      //     },
-      //     dates: new Date(res[i].date),
-      //
-      //   }
-      // }
-      return res;
+
+
     }
-
-
-  }
-}
 )
 
 </script>
