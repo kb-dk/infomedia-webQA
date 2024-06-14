bf8869b164494c3010b0af7f799605d41bf21d89
HalinaHolst <HalinaHolst@users.noreply.github.com>
2024-06-14 21:17:42 +0200

    Merged with master

--- src/components/NotesForm.vue
+++ src/components/NotesForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <p v-if="errorMessage.length > 0" style="color: red">{{errorMessage}}</p>
+  <p v-if="errorMessage.toString().length > 0" style="color: red">{{errorMessage}}</p>
   <form @submit.prevent>
     <div class="form">
       <div class="header">
@@ -52,43 +52,44 @@ export default defineComponent({
   methods: {
     async createPost(post: { id: any; body: any }) {
       if (this.batch) {
-        try{
-        let response;
-        let url = `/api/batches/${this.batch.id}`;
-        switch (this.notesType) {
-          case NotesType.BATCHNOTE:
-            url += "/notes-to-batch?username=gui";
-            break;
-          case NotesType.EDITIONNOTE:
-            if (this.newspaper) {
-              url += `/newspapers/${this.newspaper.id}/notes-to-section?username=gui`;
-            }
-            break;
-          case NotesType.SECTIONNOTE:
-            if (this.newspaper && this.sectiontitle) {
-              url += `/newspapers/${this.newspaper.id}/notes-to-pages?username=gui&section_title=${this.sectiontitle}`;
-            }
-            break;
-          case NotesType.PAGENOTE:
-            if (this.newspaper && this.sectiontitle && this.pagenumber) {
-              url += `/newspapers/${this.newspaper.id}/notes-to-pages?username=gui&section_title=${this.sectiontitle}&page_number=${this.pagenumber}`;
-            }
-            break;
-          default:
-            console.log("Incorrect notestype");
-            return;
-        }
-        response = await axios({
-          method: "POST",
-          url: url,
-          data: post.body,
-          headers: {'Content-Type': 'text/plain'}
-        });
-        this.posts.push({note:post.body,id:response.data.note_id_created,batch_id:this.batch.id})
-        }catch (error){
+        try {
+          let response;
+          let url = `/api/batches/${this.batch.id}`;
+          switch (this.notesType) {
+            case NotesType.BATCHNOTE:
+              url += "/notes-to-batch?username=gui";
+              break;
+            case NotesType.EDITIONNOTE:
+              if (this.newspaper) {
+                url += `/newspapers/${this.newspaper.id}/notes-to-section?username=gui`;
+              }
+              break;
+            case NotesType.SECTIONNOTE:
+              if (this.newspaper && this.sectiontitle) {
+                url += `/newspapers/${this.newspaper.id}/notes-to-pages?username=gui&section_title=${this.sectiontitle}`;
+              }
+              break;
+            case NotesType.PAGENOTE:
+              if (this.newspaper && this.sectiontitle && this.pagenumber) {
+                url += `/newspapers/${this.newspaper.id}/notes-to-pages?username=gui&section_title=${this.sectiontitle}&page_number=${this.pagenumber}`;
+              }
+              break;
+            default:
+              console.log("Incorrect notestype");
+              return;
+          }
+          response = await axios({
+            method: "POST",
+            url: url,
+            data: post.body,
+            headers: {'Content-Type': 'text/plain'}
+          });
+          this.posts.push({note: post.body, id: response.data.note_id_created, batch_id: this.batch.id})
+        } catch (error) {
           console.log(error)
           this.errorMessage = "Unable to delete a note";
         }
+      }
     },
     removePost(post: { id: any;batch_id:any;},index:number) {
       try{
