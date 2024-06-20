08e57fd6d5cc5def4ffb300b759eea9bd3376489
Patrick Bruun <pabr@kb.dk>
2024-06-20 14:48:53 +0200

    pages object contains correct index

--- src/components/PageTable.vue
+++ src/components/PageTable.vue
@@ -64,31 +64,19 @@ export default defineComponent({
     pagesFileName: {
       immediate: true,
       handler(){
-        console.log(this.pagesFileName)
         for (let i = 0; i < this.pagesFileName.length; i++) {
           let section= this.getSectionName(this.pagesFileName[i])
           let page= this.getPageNumber(this.pagesFileName[i])
-          let fileObject = {src:this.pagesFileName[i],index:i,Section:section[1],Page:page[1],sectionName:section[0],pageName:page[0]}
-          // console.log(fileObject);
+          let fileObject = {src:this.pagesFileName[i],Section:section[1],Page:page[1],sectionName:section[0],pageName:page[0]}
           this.pages.push(fileObject);
         }
-        console.log("sorted")
-        // console.log(this.sortBySectionAndPage(this.pages))
         this.pages = this.sortBySectionAndPage(this.pages);
-        // let grouped = Object.groupBy(this.pages,({Section})=>{
-        //   return Section
-        // })
-        // this.pages = this.pages.sort((a,b) => {
-        //   return a.Section >= b.Section?4:1 - a.Page >= b.Page ? -2:0;
-        // })
-        // console.log(grouped)
-        // this.pages = this.pages.sort((a,b)=>{return a.Section -b.Section })
+
       }
     }
   },
   methods: {
     rowClicked(e) {
-      // console.log(event)
       this.focusedPage = e.index
       this.rowClick(e.src)
     },
@@ -117,14 +105,16 @@ export default defineComponent({
             const less = arr[j+1];
             arr[j+1] = arr[j];
             arr[j] = less
-
           }
           else if(arr[j].Section === arr[j+1].Section && arr[j].Page > arr[j+1].Page){
             const less = arr[j+1];
             arr[j+1] = arr[j];
-            arr[j] = less
+            arr[j] = less;
+
           }
+          arr[j].index = j;
         }
+
       }
       return arr;
     }
