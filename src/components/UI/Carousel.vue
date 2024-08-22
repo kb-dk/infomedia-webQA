e47a45fdbbe250537cc13977a38424518679371d
Patrick Bruun <pabr@kb.dk>
2024-08-22 13:35:11 +0200

    fixed zoom pdf name

--- src/components/UI/Carousel.vue
+++ src/components/UI/Carousel.vue
@@ -42,7 +42,7 @@
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title text-dark" id="pdfModalLabel">PDF - {{ selectedPdf }}</h5>
+          <h5 class="modal-title text-dark" id="pdfModalLabel">PDF - {{ selectedPdf.name }}</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
